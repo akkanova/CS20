2d0a1b0eabfa5d35e8eb4e5b7c5c9874f8cc5dce
Akkanova <alfredrickfantone@gmail.com>
2023-04-21 13:49:41 -0600

    Start Final Project

---
+++ Final Project/src/Common/Graphics.java
@@ -0,0 +1,17 @@
+package Common;
+
+// This a utility class designed for formatting
+// CLI elements and designs.
+public class Graphics {
+    public static String wrapString(String input, int max) {
+        // I refuse to use Apache Commons
+        // to solve this problem
+
+        // Use Regex to solve this problem;
+        throw new RuntimeException();
+    }
+
+    public static String addBorder(String input, int width) {
+        throw new RuntimeException();
+    }
+}
