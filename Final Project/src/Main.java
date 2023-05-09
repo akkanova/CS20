3cedd910d1f8b3869f5d1c816f9b943d3601360d
Akkanova <alfredrickfantone@gmail.com>
2023-05-09 13:59:43 -0600

    Add icon to Jframe

--- Final Project/src/Main.java
+++ Final Project/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        TetrisWindow window = new TetrisWindow();
+        window.run();
     }
 }
--- Final Project/src/TetrisWindow.java
+++ Final Project/src/TetrisWindow.java
@@ -1,16 +1,25 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class TetrisWindow extends JFrame {
 
     public TetrisWindow() {
-        this(500, 600, "Java Tetris");
+        this(600, 500, "Java Tetris");
     }
 
     public TetrisWindow(int width, int height, String title) {
         super(title);
         super.setSize(width, height);
         super.setLocationByPlatform(true);
+        super.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+        // Load Images
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Image icon = toolkit.createImage("sample.jfif");
+        super.setIconImage(icon);
     }
 
+    public void run() {
+        super.setVisible(true);
+    }
 }
