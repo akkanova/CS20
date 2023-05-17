591f8e8679d74656dfbd45d95a49d7c04c45b09a
Akkanova <alfredrickfantone@gmail.com>
2023-05-17 01:01:09 -0600

    Setup IntelliJ GUI designer

--- Final Project/src/Main.java
+++ Final Project/src/Main.java
@@ -1,5 +1,4 @@
 import javax.swing.*;
-import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,7 +8,7 @@ public class Main {
         SwingUtilities.invokeLater(() -> {
             // Load all the resources first.
             Resources.load();
-            new TetrisMenu();
+            new GameMenu();
         });
     }
 }
